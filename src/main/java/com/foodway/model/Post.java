9ac59a41adfcf66fc44da8ea46af74a590fe3da5
Yashasv-G <yashasvgupta100@gmail.com>
2024-12-19 23:41:36 +0530

    Create Post.java

---
+++ src/main/java/com/foodway/model/Post.java
@@ -0,0 +1,16 @@
+package com.foodway.model;
+
+public class Post {
+    private String title;
+    private String description;
+    private String imageUrl;
+
+    // Constructor, Getters, Setters
+    public Post(String title, String description, String imageUrl) {
+        this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    // Getters and Setters
+}

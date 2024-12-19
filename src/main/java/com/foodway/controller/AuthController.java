f4a93553548e3828a3d313f1f8fcd9d0922bd63f
Yashasv-G <yashasvgupta100@gmail.com>
2024-12-19 22:41:35 +0530

    Create AuthController.java

---
+++ src/main/java/com/foodway/controller/AuthController.java
@@ -0,0 +1,12 @@
+package com.foodway.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}

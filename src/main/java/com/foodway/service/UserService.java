b877c93a84bfc234bc665b823253d484f0f8941c
Yashasv-G <yashasvgupta100@gmail.com>
2025-01-09 10:34:21 +0530

    Update UserService.java

--- src/main/java/com/foodway/service/UserService.java
+++ src/main/java/com/foodway/service/UserService.java
@@ -1,1 +1,22 @@
 
+package com.foodway.foodway.service;
+
+import com.foodway.foodway.model.User;
+import com.foodway.foodway.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User authenticateUser(User user) {
+        return userRepository.findByUsername(user.getUsername());
+    }
+}

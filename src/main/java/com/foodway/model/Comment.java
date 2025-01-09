7ad6fae882e877a5d9d85878664a94f9483b11a9
Yashasv-G <yashasvgupta100@gmail.com>
2025-01-09 10:27:58 +0530

    Create Comment.java

---
+++ src/main/java/com/foodway/model/Comment.java
@@ -0,0 +1,40 @@
+package com.foodway.foodway.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String content;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}

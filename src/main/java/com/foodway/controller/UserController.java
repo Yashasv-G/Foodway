130e7f894e7dd3e601c1416aae90133673d9ad78
Yashasv-G <yashasvgupta100@gmail.com>
2025-01-09 10:33:58 +0530

    Create UserController.java

---
+++ src/main/java/com/foodway/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.foodway.foodway.controller;
+
+import com.foodway.foodway.model.User;
+import com.foodway.foodway.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestBody User user) {
+        return userService.authenticateUser(user);
+    }
+}

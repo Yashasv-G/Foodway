0701b296076ae10ea470d7a5d06ea68dc59f73f9
Yashasv-G <yashasvgupta100@gmail.com>
2025-01-09 10:32:49 +0530

    Update RecipeService.java

--- src/main/java/com/foodway/service/RecipeService.java
+++ src/main/java/com/foodway/service/RecipeService.java
@@ -1,1 +1,30 @@
 
+package com.foodway.foodway.service;
+
+import com.foodway.foodway.model.Recipe;
+import com.foodway.foodway.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RecipeService {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    public List<Recipe> getAllRecipes() {
+        return recipeRepository.findAll();
+    }
+
+    public Recipe createRecipe(Recipe recipe) {
+        return recipeRepository.save(recipe);
+    }
+
+    public Recipe getRecipeById(Long id) {
+        Optional<Recipe> recipe = recipeRepository.findById(id);
+        return recipe.orElse(null);
+    }
+}
